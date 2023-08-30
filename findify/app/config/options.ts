7ad91ee3dfce6885a736b1d37bf69c7b71d7e9d7
yehoki <patsenn2000@gmail.com>
2023-08-30 18:55:19 +0100

    Fix secret variable

--- findify/app/config/options.ts
+++ findify/app/config/options.ts
@@ -4,10 +4,9 @@ import SpotifyProvider from 'next-auth/providers/spotify';
 import { URLSearchParams } from 'url';
 
 const spotifyScope =
-  'playlist-modify-public playlist-modify-private user-read-recently-played user-read-playback-state user-top-read user-modify-playback-state user-read-currently-playing user-follow-read playlist-read-private user-read-email user-read-private user-library-read playlist-read-collaborative';
-
+    'playlist-modify-public playlist-modify-private user-read-recently-played user-read-playback-state user-top-read user-modify-playback-state user-read-currently-playing user-follow-read playlist-read-private user-read-email user-read-private user-library-read playlist-read-collaborative';
 export const options: NextAuthOptions = {
-  secret: process.env.NEXT_AUTH_SECRET as string,
+  secret: process.env.NEXTAUTH_SECRET as string,
   providers: [
     SpotifyProvider({
       clientId: process.env.SPOTIFY_CLIENTID as string,
