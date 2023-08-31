3cf6c6545d6865c1a6c1663efd5785ad73995950
yehoki <patsenn2000@gmail.com>
2023-08-31 11:09:16 +0100

    Add empty gap for time period switch in user tracks empty state

--- findify/app/components/tracks/EmptyTracksState.tsx
+++ findify/app/components/tracks/EmptyTracksState.tsx
@@ -10,6 +10,7 @@ const EmptyTracksState: React.FC<EmptyTracksStateProps> = ({ home }) => {
   const tracks = [1, 2, 3, 4, 5, 6, 7, 8];
   return (
     <div>
+      <div className="h-10 w-full"></div>
       <div className="h-8 p-2 mb-4"></div>
       <div className="grid gap-4 grid-rows-1 grid-flow-col">
         {tracks.map((track) => (
