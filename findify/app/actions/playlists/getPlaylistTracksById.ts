ab962dd2f67bbcc9b01214104e034b932852f967
Patryk <patsenn2000@gmail.com>
2024-09-25 21:04:11 +0100

    Implement recursive track population

--- findify/app/actions/playlists/getPlaylistTracksById.ts
+++ findify/app/actions/playlists/getPlaylistTracksById.ts
@@ -4,11 +4,19 @@ import getUserSession from '../user/getUserSession';
 
 export default async function getPlaylistTracksById(
   playlistId: string,
-  session: Session
+  session: Session,
+  next: string | null = null,
+  trackArray: TrackObject[] | null = null
 ) {
   try {
     const spotifyBaseURL = 'https://api.spotify.com/v1/playlists';
-    const res = await fetch(`${spotifyBaseURL}/${playlistId}/tracks`, {
+    let connectionString;
+    if (next === null) {
+      connectionString = `${spotifyBaseURL}/${playlistId}/tracks`;
+    } else {
+      connectionString = next;
+    }
+    const res = await fetch(connectionString, {
       method: 'GET',
       headers: {
         Authorization: `Bearer ${session.user.accessToken}`,
@@ -17,14 +25,33 @@ export default async function getPlaylistTracksById(
     if (!res.ok) {
       return null;
     }
-    const playlistInfo = await res.json();
-    const playlistTracks: TrackObject[] = playlistInfo.items.map(
-      (item: { track: TrackObject }) => item.track
+    const playlistInfo: {
+      next: string | null;
+      items: {
+        track: TrackObject;
+      }[];
+    } = await res.json();
+    let playlistTracks: TrackObject[] = playlistInfo.items.map(
+      (item) => item.track
     );
     if (!playlistTracks) {
       return null;
     }
-    return playlistTracks;
+    if (trackArray !== null) {
+      // If we're passing in an existing track array via recursion, add all tracks to it
+      playlistTracks = trackArray.concat(playlistTracks);
+    }
+
+    if (playlistInfo.next === null) {
+      return playlistTracks;
+    } else {
+      return getPlaylistTracksById(
+        playlistId,
+        session,
+        playlistInfo.next,
+        playlistTracks
+      );
+    }
   } catch (err) {
     console.error(err);
     return null;
